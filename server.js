8a92e3d33d1e5e279a2e1a6ba790ff8a8864d2d6
ianbridges <megabridge12@gmail.com>
2022-07-21 18:08:49 -0500

    data retrieval

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ app.use(express.json());
 
 
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!');
